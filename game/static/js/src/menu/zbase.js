a845a085286207bf47b8041593870c056fad988a
GeT-LeFt <913667252hcx@gmail.com>
2023-01-09 23:00:48 +0800

    write menu and playground scene

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -52,38 +52,3 @@ class AcGameMenu {
         this.$menu.hide();
     }
 }
-class AcGamePlayground {
-    constructor(root) {
-        this.root = root;
-        this.$playground = $(`<div>Game Start!</div>`);
-
-        this.hide();
-        this.root.$ac_game.append(this.$playground);
-
-        this.start();
-    }
-
-    start() {
-    }
-
-    show() {    // 打开playground界面
-        this.$playground.show();
-    }
-
-    hide() {    // 关闭playground界面
-        this.$playground.hide();
-    }
-}
-class AcGame {
-    constructor(id) {
-        this.id = id;
-        this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);   // 创建菜单界面
-        this.playground = new AcGamePlayground(this);   // 创建游戏界面
-
-        this.start();
-    }
-
-    start() {
-    }
-}
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,13 @@
 class AcGame {
     constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);   // 创建菜单界面
+        this.playground = new AcGamePlayground(this);   // 创建游戏界面
 
+        this.start();
+    }
+
+    start() {
     }
 }
